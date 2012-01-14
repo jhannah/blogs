c97662796cbbb06065826fc125e1212a7f924748
Jay Hannah <jhannah@mutationgrid.com>
2012-01-13 20:54:38 -0600

    Merged new emails into single big mbox.  :)

--- old/roberta/mhonarc/go.sh
+++ old/roberta/mhonarc/go.sh
@@ -1,10 +1,9 @@
 
-RJ=/home/jhannah/public_html/roberta2
 TITLE="Roberta Jenkins Mail Archive"
 
-/home/jhannah/MHonArc/bin/mhonarc \
-   -outdir $RJ \
+mhonarc \
+   -outdir cooked \
    -title "$TITLE" -ttitle "$TITLE" \
-   -spammode -rcfile $RJ/mhonarc.mrc \
-   $RJ/raw/* 
+   -spammode -rcfile mhonarc.mrc \
+   raw/* 
 
